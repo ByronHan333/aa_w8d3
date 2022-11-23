9016d5e852b6b5f2ee41e4080d72f5ae897152f8
Nathan Kwon <nathan.kwon.8.18@gmail.com>
2022-11-23 14:35:52 -0800

    add numbers

---
+++ add_numbers.js
@@ -0,0 +1,27 @@
+const readline = require('readline');
+
+const rl = readline.createInterface( {
+    input: process.stdin,
+    output: process.stdout
+});
+
+const completionCallback = (sum) => {
+    console.log(`total sum: ${sum}`);
+}
+
+const addNumbers = (sum, numsLeft, completionCallback) => {
+    if (numsLeft === 0) {
+        completionCallback(sum);
+    } else {
+        rl.question ("Enter a number.", num => {
+            num = parseInt(num);
+            sum += num;
+            numsLeft--;
+            console.log(`Current Sum: ${sum}`);
+            rl.close();
+        });
+        addNumbers (sum, numsLeft, completionCallback);
+    }
+}
+
+addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
